498a36c2cab25c64579905d6377fc7fb7c4682f0
Vargas-Veronica <vargas.veronica55@gmail.com>
2024-09-16 16:59:52 -0300

    Estructura estática de la pagina destacados

--- app/src/main/java/com/example/planetsuperheroes/Products_Dc.java
+++ app/src/main/java/com/example/planetsuperheroes/Products_Dc.java
@@ -7,32 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Products_Dc extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_products_dc);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button goToSecondActivityButton = findViewById(R.id.goToSecondActivityButton);
-        goToSecondActivityButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Crea un Intent para ir a SecondActivity
-                Intent intent = new Intent(MainActivity.this, Products_Dc.class);
-                startActivity(intent);  // Inicia la SecondActivity
-            }
-        });
-
-
     }
 }
