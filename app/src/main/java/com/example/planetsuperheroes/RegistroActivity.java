24e45aeb087e9b5575e05c4924ad4b2d77a70b97
gonzalo-olmedo <gonzaloabelolmedo8@gmail.com>
2024-09-18 01:35:36 -0300

    Subo activity Registro

--- app/src/main/java/com/example/planetsuperheroes/RegistroActivity.java
+++ app/src/main/java/com/example/planetsuperheroes/RegistroActivity.java
@@ -8,14 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_registro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tvRegisterTitleText), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
