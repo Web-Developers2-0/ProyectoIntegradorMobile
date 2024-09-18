23774f2edb1ce4f19d8a52ba6cd483684a36bd61
Emilce_Robles <emi.nrobles@gmail.com>
2024-09-18 00:34:13 -0300

    se agrega relacion de activities principales

--- app/src/main/java/com/example/planetsuperheroes/LoginActivity.java
+++ app/src/main/java/com/example/planetsuperheroes/LoginActivity.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -16,7 +15,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login); // Cambia activity_main a activity_login
+        setContentView(R.layout.activity_login);
 
         // Inicializar vistas
         editTextEmail = findViewById(R.id.editTextEmail);
--- app/src/main/java/com/example/planetsuperheroes/MainActivity.java
+++ app/src/main/java/com/example/planetsuperheroes/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.planetsuperheroes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,10 +16,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Ajustar márgenes del layout para las barras del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Encontrar el botón de iniciar sesión y configurar el OnClickListener
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear un Intent para abrir la LoginActivity
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent); // Iniciar la nueva actividad
+            }
         });
     }
 }
