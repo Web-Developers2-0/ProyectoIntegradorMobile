940220ee7d6b7fbe1597a87f8b8a7f1706063d22
Santiago Bertorello <santinbertorello@gmail.com>
2024-09-19 02:01:05 -0300

    cambios para correcciones

--- app/src/main/java/com/example/planetsuperheroes/MainActivity.java
+++ app/src/main/java/com/example/planetsuperheroes/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.planetsuperheroes;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
@@ -14,12 +15,13 @@ public class MainActivity extends AppCompatActivity {
     private Button btnOpenRegister;
     private Button btnOpenLogin;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Settings), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
