05011b7aaefc9882b6ef55654ca757526052a5b1
PaulaVelezz <velezpaula.a@gmail.com>
2024-09-19 03:35:31 -0300

    navegacion/integracion activities

--- app/src/main/java/com/example/planetsuperheroes/HomeActivity.java
+++ app/src/main/java/com/example/planetsuperheroes/HomeActivity.java
@@ -3,18 +3,29 @@ package com.example.planetsuperheroes;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+    private ImageButton btnUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
+        btnUser = findViewById(R.id.btnUser);
+
+        btnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, infoPersonal.class);
+                startActivity(intent);
+            }
+        });
 
         // Configura el listener para el botón de Marvel
         ImageButton btnMarvel = findViewById(R.id.btnMarvel);
