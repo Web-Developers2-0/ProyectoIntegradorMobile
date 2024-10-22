1a221e29ed971c94e43147564077df3972f11de8
PaulaVelezz <velezpaula.a@gmail.com>
2024-10-21 23:50:44 -0300

    actualizo con develop

--- app/src/main/java/com/example/planetsuperheroes/infoPersonal.java
+++ app/src/main/java/com/example/planetsuperheroes/infoPersonal.java
@@ -1,6 +1,8 @@
 package com.example.planetsuperheroes;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,15 +10,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
-
-
-
 public class infoPersonal extends AppCompatActivity {
+
+    private EditText editTextName, editTextlastName, editTextEmail, editTextAdress;
+    private Button btnGuardarCambios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_info_personal);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
